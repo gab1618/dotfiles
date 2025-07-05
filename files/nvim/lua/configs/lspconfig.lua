ca2beac477f3b65a51ef4693ed20084fbe2688be
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2025-07-05 16:01:35 -0300

    feat(nvim): add rust analyser config

--- files/nvim/lua/configs/lspconfig.lua
+++ files/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,17 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "rust_analyser" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
+
+vim.lsp.config('rust_analyzer', {
+  settings = {
+    ['rust-analyzer'] = {
+      diagnostics = {
+        enable = false;
+      }
+    }
+  }
+})
+require('lspconfig').rust_analyzer.setup{}
