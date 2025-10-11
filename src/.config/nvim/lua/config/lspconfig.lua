ece67fabdc3af79af8823e95dfe5a5b7cd3d2518
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2025-10-11 14:15:01 -0300

    feat(nvim): add more lsp config

--- src/.config/nvim/lua/config/lspconfig.lua
+++ src/.config/nvim/lua/config/lspconfig.lua
@@ -1,8 +1,6 @@
 local servers = { "lua_ls", "html", "cssls", "rust_analyser", "ts_ls", "svelte", "elixirls", "astro", "texlab" }
 vim.lsp.enable(servers)
 
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP code action" })
-
 vim.lsp.config("rust_analyzer", {})
 
 vim.lsp.config("ts_ls", {})
@@ -14,3 +12,9 @@ vim.lsp.config("astro", {})
 vim.lsp.config("texlab", {})
 
 vim.lsp.config("lua_ls", {})
+
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP code action" })
+vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, { desc = "Format" })
+vim.keymap.set("n", "gd", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+vim.keymap.set("n", "ds", vim.diagnostic.setloclist, { desc = "Go to implementation" })
