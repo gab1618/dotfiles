e675ca5b0cd303a85456b627493cd385657d466a
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2025-10-18 16:18:50 -0300

    chore(nvim): remove command mode mapping

--- src/config/nvim/lua/mappings.lua
+++ src/config/nvim/lua/mappings.lua
@@ -1,6 +1,5 @@
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "kj", "<ESC>")
 map("n", "<leader>/", "gcc", { desc = "toggle comment", remap = true })
 map("v", "<leader>/", "gc", { desc = "toggle comment", remap = true })
