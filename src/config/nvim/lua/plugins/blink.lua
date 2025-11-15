b83857b9a4724fa0b41c7bdf00172d57f5fb97e9
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2025-11-15 12:31:59 -0300

    chore(nvim): return ctrl-n to accept completion

--- src/config/nvim/lua/plugins/blink.lua
+++ src/config/nvim/lua/plugins/blink.lua
@@ -10,6 +10,7 @@ return {
         ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
         ['<C-e>'] = { 'hide', 'fallback' },
         ["<CR>"] = { 'accept', 'fallback' },
+        ["<C-n>"] = { 'select_and_accept', 'fallback' },
 
         ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
         ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
